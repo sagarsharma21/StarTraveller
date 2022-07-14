3c8160f6cdbe534c1decc9be7816ba4b90f186c4
sagarsharma21 <007sharma.sagar@gmail.com>
2022-07-14 23:54:44 +0530

    -Map working fixed
    Added marker component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,32 @@
-
+import * as React from 'react';
+import Map, { Marker } from 'react-map-gl';
 
 function App() {
+  
   return (
-    <div className="App">
-    HelloWorld!
-    </div>
+    <Map
+    mapboxAccessToken={process.env.REACT_APP_MYURL }
+        initialViewState={{
+          latitude:28.612,
+          longitude:77.229,
+          zoom:2.5
+        }}
+style={{width:"100vw",height:"100vh"}}
+mapStyle="mapbox://styles/mapbox/streets-v9"
+    >
+      {/* //markerstart */}
+      <Marker longitude={77.229} latitude={28.612}
+        color={'#FF0000'} offsetLeft={-20} anchor="center">
+          <img src=''/><div>YOU are Here</div>
+        </Marker>
+      {/* //markerend */ }
+    
+
+    </Map>
   );
+   <div>
+  HelloWorld!
+  </div> 
 }
 
 export default App;
