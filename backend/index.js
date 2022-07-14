f80b104f7b20c8d5d3741feee4ec077af48bc55a
sagarsharma21 <007sharma.sagar@gmail.com>
2022-07-14 21:13:39 +0530

    -Connect to MongoDB
    -Add and configure dotenv package

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
 const express =require ("express");
 const mongoose = require ("mongoose");
+const dotenv = require("dotenv");
 const app =express();
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_CONNECT,{useNewUrlParser:true})
+.then(()=>{
+    console.log("MongoDB connected successfully!");
+}).catch((err)=>console.log(err));
 
 app.listen(9000, ()=>{
     console.log("Backend server is running!");
