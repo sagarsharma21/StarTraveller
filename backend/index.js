881da97c5e211e92d774f186516377c6709492bf
sagarsharma21 <007sharma.sagar@gmail.com>
2022-07-14 21:37:29 +0530

    -Access data using Postman

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,22 @@ const express =require ("express");
 const mongoose = require ("mongoose");
 const dotenv = require("dotenv");
 const app =express();
+const pinRoute = require("./routes/pins");
 
 dotenv.config();
+
+app.use(express.json());
+
 mongoose.connect(process.env.MONGO_CONNECT,{useNewUrlParser:true})
 .then(()=>{
     console.log("MongoDB connected successfully!");
 }).catch((err)=>console.log(err));
 
 
+app.use("/api/pins", pinRoute);
+
+
 app.listen(9000, ()=>{
     console.log("Backend server is running!");
 });
+
